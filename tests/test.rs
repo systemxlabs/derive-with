9d71190db7f398f07ce539bfe8417907521228ce
Linwei Zhang <lewiszlw520@gmail.com>
2023-10-19 22:50:02 +0800

    Add more tests

--- tests/test.rs
+++ tests/test.rs
@@ -37,6 +37,19 @@ fn test_struct_with_generic() {
     assert_eq!(foo.b, "1".to_string());
 }
 
+#[derive(with, Default)]
+pub struct Foo3<'a, T: Default> {
+    pub a: T,
+    pub b: &'a str,
+}
+#[test]
+fn test_struct_with_lifetime_and_generic() {
+    let foo = Foo3::<i32>::default().with_a(1).with_b("1");
+    assert_eq!(foo.a, 1);
+    assert_eq!(foo.b, "1");
+}
+
+
 /// Tuple Struct Tests
 #[derive(with, Default)]
 pub struct Bar(i32, String);
@@ -63,4 +76,13 @@ fn test_tuple_struct_with_generic() {
     let bar = Bar2::<String>::default().with_0(1).with_1("1".to_string());
     assert_eq!(bar.0, 1);
     assert_eq!(bar.1, "1".to_string());
+}
+
+#[derive(with, Default)]
+pub struct Bar3<'a, T: Default>(T, &'a str);
+#[test]
+fn test_tuple_struct_with_lifetime_and_generic() {
+    let bar = Bar3::<i32>::default().with_0(1).with_1("1");
+    assert_eq!(bar.0, 1);
+    assert_eq!(bar.1, "1");
 }
